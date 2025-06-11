b289479ab2a206ad121c4ab4d1d86faa2675907e
Normal2169 <vladimerp744@gmail.com>
2025-06-11 12:34:08 +0500

    МИГРАЦИИИИИИИ!

--- models.py
+++ models.py
@@ -1,27 +1,22 @@
 from extensions import db
+
 class Drivers(db.Model):
-    __tablename__ = 'Drivers'
+    __tablename__ = 'drivers'
     id = db.Column(db.Integer, primary_key=True)
-    Full_Name = db.Column(db.String(100), nullable=False)
-    Car = db.Column(db.String(100), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
     rating = db.Column(db.Integer)
     is_available = db.Column(db.Boolean, default=True)
     def __repr__(self):
         return f'<Drivers {self.title}>'
-class Orders(db.Model):
-    __tablename__ = 'Orders'
-    id = db.Column(db.Integer, primary_key=True)
-    Client = db.Column(db.String(100), nullable=False)
-    Date = db.Column(db.String(25), nullable=False)
-    route =  db.Column(db.String(25), nullable=False)
-    def __repr__(self):
-        return f'<Orders {self.title}>'
+
 class Cars(db.Model):
-    __tablename__ = 'Cars'
+    __tablename__ = 'cars'
     id = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String(100), nullable=False)
-    Manufacturer = db.Column(db.String(100), nullable=False)
-    Number = db.Column(db.String(100), nullable=False)
-    Color = db.Column(db.String(25))
+    name = db.Column(db.String(100), nullable=False)
+    manufacturer = db.Column(db.String(100), nullable=False)
+    number = db.Column(db.String(100), nullable=False)
+    color = db.Column(db.String(100), nullable=False)
+    is_available = db.Column(db.Boolean, default=True)
     def __repr__(self):
-        return f'<Cars {self.title}>'
+        return f'<Drivers {self.title}>'
+    
